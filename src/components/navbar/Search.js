04950001eb52a328fb95ff2deab6455ac89928af
sayeef-rahman <sayeef.cse@gmail.com>
2022-12-10 14:39:25 +0600

    search input action dispatched & working

--- src/components/navbar/Search.js
+++ src/components/navbar/Search.js
@@ -1,13 +1,19 @@
 import { useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { searched } from "../../features/filter/filterSlice";
 
 export default function Search() {
   const { search } = useSelector((state) => state.filter);
+  const dispatch = useDispatch();
   const [input, setInput] = useState(search);
   console.log("search: ", search);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(searched(input));
+  };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
         className="outline-none border-none mr-2"
         type="search"
