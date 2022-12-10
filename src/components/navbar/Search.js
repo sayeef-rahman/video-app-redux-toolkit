79b27d9e5461589039cbfe5f408dc2a40380249a
sayeef-rahman <sayeef.cse@gmail.com>
2022-12-10 19:41:56 +0600

    redirect after search

--- src/components/navbar/Search.js
+++ src/components/navbar/Search.js
@@ -1,15 +1,23 @@
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useMatch, useNavigate } from "react-router-dom";
 import { searched } from "../../features/filter/filterSlice";
 
 export default function Search() {
   const { search } = useSelector((state) => state.filter);
   const dispatch = useDispatch();
   const [input, setInput] = useState(search);
-  console.log("search: ", search);
+  const navigate = useNavigate();
+  
+  // use match checks if we are in the given parameter route or not
+  const match = useMatch('/');
+  
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(searched(input));
+
+    //if the user is not in home page then after search we should redirect user to home;
+    !match && navigate("/"); 
   };
 
   return (
